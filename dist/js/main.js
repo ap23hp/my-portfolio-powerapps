d9978e51a3c5c71d8793be311e84a607bb760774
Apra <apra.khanna2304@gmail.com>
2025-10-24 15:37:33 +0530

    portfolio in progress

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,13 +1,15 @@
-//select DOM items
+// Select DOM Items
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
 const navItems = document.querySelectorAll(".nav-item");
 
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
+
 function toggleMenu() {
   if (!showMenu) {
     menuBtn.classList.add("close");
@@ -15,6 +17,7 @@ function toggleMenu() {
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
     navItems.forEach((item) => item.classList.add("show"));
+
     // Set Menu State
     showMenu = true;
   } else {
